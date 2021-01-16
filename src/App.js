f2865bbbbafd2bef62e61c2f607bc5e21870698f
Angel_Brown <arbrown110@gmail.com>
2021-01-16 11:21:38 -0500

    app.js.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
 import mama from './mama.gif';
 import './App.css';
+import CategoriesContainer from './containers/CategoriesContainer';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import Home from './components/Home'
+import Navigation from './components/Navigation'
+import IngredientsContainer from './containers/IngredientsContainer'
+import IngredientMealsList from './containers/IngredientMealsList'
 
 function App() {
   return (
@@ -18,6 +24,18 @@ function App() {
           Learn React
         </a>
       </header>
+      <Router>
+      <div className="App">
+        <Navigation />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/categories" component={CategoriesContainer} />
+          <Route exact path="/ingredients" component={IngredientsContainer} />
+          <Route exact path="/ingredients/:id" component={IngredientMealsList} />
+
+        </Switch>
+      </div>
+      </Router>
     </div>
   );
 }
