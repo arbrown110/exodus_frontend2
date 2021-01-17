0f3454f382a19c1879f57b0dd54897f85f19ea2b
Angel_Brown <arbrown110@gmail.com>
2021-01-17 16:15:18 -0500

    edits made on links,and about.js.

---
+++ src/components/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const About = () => {
+    return (
+        <div className="about">
+           This is a little of my experience going to the country of Sierre Leone. Follow me on my journey of how I navigating the country with the people , culture and food.
+
+        </div>
+    )
+}
+
+export default About
