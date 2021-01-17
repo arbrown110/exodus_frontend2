eab0c1db9913f90b4337af54256e13b25d5b39c0
Angel_Brown <arbrown110@gmail.com>
2021-01-17 17:13:15 -0500

    edits to home.js.

--- src/App.js
+++ src/App.js
@@ -1,47 +1,21 @@
 import mama from './mama.gif';
 import './App.css';
 import AdventuresContainer from './containers/AdventuresContainer';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-import Home from './components/Home'
-import Navigation from './components/Navigation'
-import EventsContainer from './containers/EventsContainer'
-import EventsList from './containers/EventsList'
-
 import React from 'react';
 
 class App extends React.Component {
   render() {
   return (
-    <div className="App">
+    <div style={{ backgrounsImage: `url("https://www.rainbowtours.co.uk/upload-files/blog-sections/section-123_1236.jpg")`}}>
+      <div className="App">
       <header className="App-header">
         <img src={mama} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <Router>
-      <div className="App">
-        <Navigation />
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/adventures" component={AdventuresContainer} />
-          <Route exact path="/events" component={EventsContainer} />
-          <Route exact path="/events/:id" component={EventsList} />
-
-        </Switch>
+        <AdventuresContainer />
+        </header>
       </div>
-      </Router>
     </div>
-  );
-}
-}
+
+
+
 
 export default App;
